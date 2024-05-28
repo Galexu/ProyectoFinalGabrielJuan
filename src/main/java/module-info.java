28c318bd8a53c20f0cfffe98370a9fd5ca69fa39
Galex <galexu@gmail.com>
2024-05-29 01:47:50 +0200

    añadidas dependencias Maven

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.iesochoa.gabrieljuan.proyectofinalgabrieljuan {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.iesochoa.gabrieljuan.proyectofinalgabrieljuan to javafx.fxml;
     exports com.iesochoa.gabrieljuan.proyectofinalgabrieljuan;
