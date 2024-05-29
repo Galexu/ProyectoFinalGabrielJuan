2e1508dc1f0af12369ad187b81e658c2f3b98743
Galex <galexu@gmail.com>
2024-05-29 02:53:46 +0200

    conexion a la base de datos basica, con una segunda forma comentada por si acaso

---
+++ src/main/java/com/iesochoa/gabrieljuan/proyectofinalgabrieljuan/dao/ConexionDB.java
@@ -0,0 +1,25 @@
+package com.iesochoa.gabrieljuan.proyectofinalgabrieljuan.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionDB {
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+//    public static void conectar() throws SQLException {
+//        try (final Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+//            System.out.println(connection.getCatalog());
+//        } catch (SQLException ex) {
+//            System.out.println("SQLException: " + ex.getMessage());
+//            System.out.println("SQLState: " + ex.getSQLState());
+//            System.out.println("VendorError: " + ex.getErrorCode());
+//        }
+//    }
+}
