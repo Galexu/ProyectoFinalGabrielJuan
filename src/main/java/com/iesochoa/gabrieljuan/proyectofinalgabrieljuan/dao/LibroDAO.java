b870a98c11074340e26f646ebf7ec94b0f836edb
Galex <galexu@gmail.com>
2024-05-31 04:12:05 +0200

    metodos obtenerLibros y actualizarLibros añadidos, por probar aun

--- src/main/java/com/iesochoa/gabrieljuan/proyectofinalgabrieljuan/dao/LibroDAO.java
+++ src/main/java/com/iesochoa/gabrieljuan/proyectofinalgabrieljuan/dao/LibroDAO.java
@@ -3,6 +3,8 @@ package com.iesochoa.gabrieljuan.proyectofinalgabrieljuan.dao;
 import com.iesochoa.gabrieljuan.proyectofinalgabrieljuan.modelo.Libro;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LibroDAO {
     public void agregarLibro(Libro libro) {
@@ -21,4 +23,44 @@ public class LibroDAO {
             e.printStackTrace();
         }
     }
+
+    public List<Libro> obtenerLibros() {
+        List<Libro> libros = new ArrayList<>();
+        String sql = "SELECT * FROM libros";
+
+        try (Connection con = ConexionDB.conectar();
+             Statement statement = con.createStatement();
+             ResultSet rs = statement.executeQuery(sql)) {
+            while (rs.next()) {
+                Libro libro = new Libro();
+                libro.setIsbn(rs.getString("isbn"));
+                libro.setTitulo(rs.getString("titulo"));
+                libro.setAutor(rs.getString("autor"));
+                libro.setAnoPublicacion(rs.getInt("ano_publicacion"));
+                libro.setGenero(rs.getString("genero"));
+                libro.setPortada(rs.getBytes("portada"));
+                libros.add(libro);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return libros;
+    }
+
+    public void actualizarLibro(Libro libro) {
+        String sql = "UPDATE libros SET titulo = ?, autor = ?, ano_publicacion = ?, genero = ?, portada = ? WHERE isbn = ?";
+
+        try (Connection con = ConexionDB.conectar(); PreparedStatement statement = con.prepareStatement(sql)) {
+            statement.setString(1, libro.getTitulo());
+            statement.setString(2, libro.getAutor());
+            statement.setInt(3, libro.getAnoPublicacion());
+            statement.setString(4, libro.getGenero());
+            statement.setBytes(5, libro.getPortada());
+            statement.setString(6, libro.getIsbn());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
